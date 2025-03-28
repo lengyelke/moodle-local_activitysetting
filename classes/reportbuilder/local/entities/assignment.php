fd751faa330fbda0cc5f6efb37365444c5ac1792
lengyelke <lengyelke@gmai.com>
2025-03-28 22:02:20 +0000

    Assignment: maxattempts -1 shown as unlimited

--- classes/reportbuilder/local/entities/assignment.php
+++ classes/reportbuilder/local/entities/assignment.php
@@ -240,7 +240,13 @@ class assignment extends base {
             ->add_joins($this->get_joins())
             ->set_type(column::TYPE_INTEGER)
             ->set_is_sortable(true)
-            ->add_field("{$assignalias}.maxattempts");
+            ->add_field("{$assignalias}.maxattempts")
+            ->add_callback(function($value) {
+                if ((int)$value === -1) {
+                    return get_string('unlimitedattempts', 'mod_assign');
+                }
+                return $value;
+            });
 
         // Assignment attemptreopenmethod column.
         $columns[] = (new column(
@@ -593,13 +599,47 @@ class assignment extends base {
 
         // Assignment maxattempts filter.
         $filters[] = (new filter(
-            number::class,
+            select::class,
             'maxattempts',
             new lang_string('maxattempts', 'mod_assign'),
             $this->get_entity_name(),
             "{$assignalias}.maxattempts"
         ))
-            ->add_joins($this->get_joins());
+            ->add_joins($this->get_joins())
+            ->set_options([
+                -1 => new lang_string('unlimitedattempts', 'mod_assign'),
+                1 => '1',
+                2 => '2',
+                3 => '3',
+                4 => '4',
+                5 => '5',
+                6 => '6',
+                7 => '7',
+                8 => '8',
+                9 => '9',
+                10 => '10',
+                11 => '11',
+                12 => '12',
+                13 => '13',
+                14 => '14',
+                15 => '15',
+                16 => '16',
+                17 => '17',
+                18 => '18',
+                19 => '19',
+                20 => '20',
+                21 => '21',
+                22 => '22',
+                23 => '23',
+                24 => '24',
+                25 => '25',
+                26 => '26',
+                27 => '27',
+                28 => '28',
+                29 => '29',
+                30 => '30',
+
+            ]);
         // Assignment attemptreopenmethod filter.
         $filters[] = (new filter(
             select::class,
