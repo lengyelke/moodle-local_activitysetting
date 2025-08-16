9e1c6b3969542d65c3f216f47420f52ee6e70f9a
Ferenc Lengyel <lengyelke@gmail.com>
2025-08-16 13:53:05 +0100

    Updating version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_activitysetting';
-$plugin->release      = '1.0';
-$plugin->version      = 2025031601;
+$plugin->release      = '1.1.0';
+$plugin->version      = 2025081600;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 500];
 $plugin->maturity     = MATURITY_STABLE;
