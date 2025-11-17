3e903596f0a4d6bca21fc3463b8bfa02c0a3354e
Ferenc Lengyel <lengyelke@gmail.com>
2025-11-17 08:33:22 +0000

    Activity plugin type = plugin name

--- classes/reportbuilder/local/entities/course_module.php
+++ classes/reportbuilder/local/entities/course_module.php
@@ -421,7 +421,10 @@ class course_module extends base {
             ->add_joins($this->get_joins())
             ->set_type(column::TYPE_TEXT)
             ->set_is_sortable(true)
-            ->add_field("{$modalias}.name");
+            ->add_field("{$modalias}.name")
+            ->add_callback(static function ($value): string {
+                return get_string('pluginname', $value) ?: $value;
+            });
 
         return $columns;
     }
